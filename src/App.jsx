8939e647a26be1fd466658fe1ead30032263d0d7
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-10-22 19:50:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <div className="justify-content-center align-items-center vh-100 bg-danger">
+      <div className="text-center bg-light">
+      <h1>Time Tracker App</h1>
+      </div>
+      <button className ="btn btn-primary">
+        Test Button
+      </button>
+    </div>
+  )
+}
+
+export default App
